041dff89359ba3f3aaaa404a8966e4000527f98d
Sam <82520167+samshahbazi@users.noreply.github.com>
2022-03-13 20:13:45 +0100

    first commit

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -11,8 +11,10 @@ use App\Http\Requests\StoreReservationRequest;
 
 class ReservationController extends Controller
 {
+    // number of total tables of the restaurant
     private int $allTablesNumber = 10;
 
+    // the store method¯\_(ツ)_/¯
     public function store(StoreReservationRequest $request)
     {
         $numberOfPersons = $request->numberOfPersons;
@@ -20,14 +22,25 @@ class ReservationController extends Controller
         $meal = $request->meal;
         $drink = $request->drink;
         $reservationStartTime =  $request->reservationStartTime;
+        
+        // check if reservation hour is between 16:00 to 20:00
+        $time = Carbon::make($reservationStartTime);
+        $date = $time->format('Y-m-d');
+        $startHour = Carbon::make($date)->format('Y-m-d 16:00');
+        $endHour = Carbon::make($date)->format('Y-m-d 20:00');
 
-        $hourCheck = (int) Carbon::make($reservationStartTime)->format('H');
-        if( ($hourCheck < 16) || ($hourCheck > 20) ){
-            return response()->json(['status' => 'error', 'message' => 'reservation time should be between 16:00 and 20:00']);
+        if (! Carbon::make($reservationStartTime)->between($startHour, $endHour)) {
+            
+            return response()->json(['status' => 'error', 'message' => "reservation time should be between $startHour and $endHour"]);
         }
 
-        $time = Carbon::make($reservationStartTime);
+        // check if reservation time is older than now(), return error.
+        if($time->lessThan(now())){
 
+            return response()->json(['status' => 'error', 'message' => "reservation time must be greater than now"]);
+        }
+
+        // get number of available table for the given time
         $numberOfFreeTables = $this->getNumberOfFreeTables($time);
 
         // check if we don't have enough table(s) then return error
@@ -35,17 +48,19 @@ class ReservationController extends Controller
         if($numberOfFreeTables < $neededTables){
             return response()->json([
                 'status' => 'error', 
+                'availablePersons' => $numberOfFreeTables * 2,
                 'message' => "There is not enough place for $numberOfPersons persons. We only have place for " 
                     . $numberOfFreeTables * 2 . ' persons from ' . Carbon::make($reservationStartTime)->format('H:i')
                     . ' to ' . Carbon::make($reservationStartTime)->addHours(2)->format('H:i'),
-                'availablePersons' => $numberOfFreeTables * 2,
             ]);
         }
         
+        // register the new email if email is not in the database. I would like to have the email of the user, so it's easier to send email or etc...
         $service = new RegisterOrLoginUserService($email);
         $user = $service->getUser();
 
         // check if user already reserved a time, we won't allow to reserve the same again but one email is able to book again with different data
+        // it's not in the project's description, I just try to prevent double submit with the exact same data...
         $Reservation = Reservation::where('user_id', $user->id)
             ->where('user_id', $user->id)
             ->where('meal_name', $meal)
@@ -61,7 +76,7 @@ class ReservationController extends Controller
             ]);                        
         }
         
-        // check if we have a free free table(s) then book the time
+        // reserve the time
         $Reservation = Reservation::create([
             'user_id'=> $user->id,
             'meal_name'=> $meal,
@@ -88,15 +103,11 @@ class ReservationController extends Controller
         //     ->where('reservation_start_time', '<', $endTime)->get();//not correct // < ok
 
         // $allDuringReservationTimes = Reservation::where('reservation_start_time', '>' , $startTime) // 
-        //     ->where('reservation_start_time', '<',  $reservationStartTime )->get(); // 
+        //     ->where('reservation_start_time', '<',  $reservationStartTime )->get(); // not correct
 
-        //should be unique
         $allReservationTimes = Reservation::where('reservation_start_time', '>' , $startTime) // 
             ->where('reservation_start_time', '<',  $endTime )->get(); // 
-        // dd($allAfterReservationTimes);
-        // dd($allDuringReservationTimes);
-        // dd($allReservationTimes);
-        // we don't count the tables are reserved for example 1 hour ago and they are not empty!
+
         $numberOfReservedTable = 0;
         foreach($allReservationTimes as $item){
             $persons = $item['number_of_reservation'];
